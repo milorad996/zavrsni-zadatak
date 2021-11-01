c488a600abf0c6167c42f36d2da9e1a724924f43
Milorad Savkovic <mips96.1@gmail.com>
2021-11-01 21:28:03 +0100

    New new

---
+++ dbConnect.php
@@ -0,0 +1,19 @@
+<?php
+    
+    $servername = "127.0.0.1";
+    $username = "academy";
+    $password = "academy";
+    $dbname = "blog";
+
+    try {
+        $connection = new PDO(
+            "mysql:host=$servername;dbname=$dbname", $username, $password
+        );
+        
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+    catch(PDOException $e)
+    {
+        echo $e->getMessage();
+    }
+?>
--- posts.php
+++ posts.php
@@ -1,6 +1,7 @@
 <?php include('header.php') ?>
 <?php include('footer.php') ?>
 <?php include('sidebar.php') ?>
+<?php include('dbConnect.php') ?>
 
 
 
@@ -26,16 +27,35 @@
 
 <body>
 
+<?php
+                
+                
+
+                $sql = "SELECT id, title, body, author, created_at FROM posts ORDER BY created_at DESC " ;
+                $statement = $connection->prepare($sql);
+                
+                $statement->execute();
+                
+                $statement->setFetchMode(PDO::FETCH_ASSOC);
+                
+                $posts = $statement->fetchAll();
+                
+                
+                
+                
+?>
 
 <main role="main" class="container">
 
     <div class="row">
 
         <div class="col-sm-8 blog-main">
-
+        <?php
+            foreach ($posts as $post) {
+         ?>
             <div class="blog-post">
-                <h2 class="blog-post-title"><a href="#">Sample blog post</a></h2>
-                <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
+                <h2 class="blog-post-title"><?php echo($post['title']) ?><a href="single-post.php"></a></h2>
+                <p class="blog-post-meta"><?php echo($post['created_at']) ?><a href="#">Mark</a></p>
 
                 <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
                 <hr>
@@ -44,13 +64,13 @@
                     <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
                 </blockquote>
                 <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-                <h2>Heading</h2>
+                <h2><a href="#"><?php echo($post['title']) ?></a></h2>
                 <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
-                <h3>Sub-heading</h3>
+                <h3><a href="#">Sub-heading</a></h3>
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
                 <pre><code>Example code block</code></pre>
                 <p>Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <h3>Sub-heading</h3>
+                <h3><a href="#">Sub-heading</a></h3>
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                 <ul>
                     <li>Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</li>
@@ -67,8 +87,8 @@
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title"><a href="#">Another blog post</a></h2>
-                <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
+                <h2 class="blog-post-title"><a href="single-post.php"><?php echo($post['title']) ?></a></h2>
+                <p class="blog-post-meta"><?php echo($post['created_at']) ?><a href="#">Jacob</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
                 <blockquote>
@@ -79,8 +99,8 @@
             </div><!-- /.blog-post -->
 
             <div class="blog-post">
-                <h2 class="blog-post-title"><a href="#">New feature</a></h2>
-                <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
+                <h2 class="blog-post-title"><a href="single-post.php"><?php echo($post['title']) ?></a></h2>
+                <p class="blog-post-meta"><?php echo($post['created_at']) ?><a href="#">Chris</a></p>
 
                 <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
                 <ul>
@@ -96,7 +116,9 @@
                 <a class="btn btn-outline-primary" href="#">Older</a>
                 <a class="btn btn-outline-secondary disabled" href="#">Newer</a>
             </nav>
-
+        <?php
+            }
+        ?>
         </div><!-- /.blog-main -->
 
         
